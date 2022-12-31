c23abfcf437ba2459486a81cfa608f8d5322777d
salman naveed <realmadridinmydna3@gmail.com>
2022-12-31 18:47:43 +0500

    added close-btn

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// const modal = document.getElementById('modal')
-
-// setTimeout(function () {
-//     modal.style.display = 'inline'
-// }, 15000)
-
-// console.log("hi salman")
-
 const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
 
 setTimeout(function () {
     modal.style.display = 'inline'
 }, 1500)
+
+modalCloseBtn.addEventListener('click', function () {
+    modal.style.display = 'none'
+})
